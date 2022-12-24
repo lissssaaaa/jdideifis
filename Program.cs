7e6a9cd8df46c522ef9f184c0375df3d4d5c9b9d
lisa <yelizaveta_07@list.ru>
2022-12-24 21:04:01 +0300

    Zadacha

--- Program.cs
+++ Program.cs
@@ -1,65 +1,24 @@
-﻿/* //Задача 2
-Console.WriteLine("Введи первое число");
-int number1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введи второе число");
-int number2 = Convert.ToInt32(Console.ReadLine());
-
-if(number1 > number2)
+﻿string[] array1 = new string[5] {"123", "23", "hello", "world" , "res"};
+string[] array2 = new string[array1.Length];
+void SecondArrayWithIF(string[] array1, string[] array2)
 {
-    Console.WriteLine("Первое число больше второго");
+    int count = 0;
+    for (int a = 0; a < array1.Length; a++)
+    {
+        if(array1[a].Length <= 3)
+        {
+            array2[count] = array1[a];
+            count++;
+        }
+    }
 }
-else if (number1 < number2)
+void PrintArray(string[] array)
 {
-    Console.WriteLine("П3ервое число больше второго");
+    for (int a = 0; a < array.Length; a++)
+    {
+        Console.WriteLine($"{array[a]}");
+    }
+    Console.WriteLine();
 }
-else
-{
-    Console.WriteLine("Оба числа равны");
-}
-Console.ReadKey();
- */
-//Задача 4
-int a = 1;
-int b = 2;
-int c = 3;
-int max, min, mid;
-
-if (a > b && a > c)
-{
-    max = a;
-    min = (b < c) ? b : c;
-}
-else if (b > a && b > c)
-{
-    max = b;
-    min = (a < c) ? a : c;
-}
-else
-{
-    max = c;
-    min = (b < a) ? a : b;
-}
-
-//Задача 6
-int g;
-Console.Write("Введите число");
-g= Console.Read();
-
-if (g % 2==1)
-{
-    Console.WriteLine("Число нечетное");
-}
-else
-{
-    Console.WriteLine("Число четное");
-}
-
-Console.Read();
-
-//Задача 8
-Console.Write("Введите одно число");
-int n = int.Parse(Console.ReadLine());
-
-for (int i = 1; i <=n; i++)
-if (i % 2== 0);
-Console.WriteLine(i);
+SecondArrayWithIF(array1, array2);
+PrintArray(array2);
